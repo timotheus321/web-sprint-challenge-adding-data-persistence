9b46f6482518e260639d16652d15d7edeb6599dd
Timothy White <bigtball321@yahoo.com>
2023-08-24 16:24:40 -0400

    wip

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,29 @@
 // build your `Resource` model here
+const knex = require('knex');
+const config = require('../../knexfile');
+const db = knex(config.development);
+
+function findResources() {
+    return db('resources'); 
+  }
+  function findResourceById(id) {
+    return db('resources').where({ resource_id: id }).first();
+  }
+  function addResource(resource) {
+    return db('resources').insert(resource);
+  }
+  function updateResource(id, resource) {
+    return db('resources').where({ resource_id: id }).update(resource);
+  }
+  
+  function deleteResource(id) {
+    return db('resources').where({ resource_id: id }).del();
+  }
+  module.exports = {
+    findResources,
+    findResourceById,
+    addResource,
+    updateResource,
+    deleteResource,
+  };
+  
--- api/resource/router.js
+++ api/resource/router.js
@@ -1,11 +1,58 @@
 // resource/router.js
 const express = require('express');
+const Resources = require('./model');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.json({ message: 'Resource endpoint' });
-});
+    Resources.findResources()
+      .then(resources => {
+        res.status(200).json(resources);
+      })
+      .catch(err => {
+        res.status(500).json({ message: 'Failed to retrieve resources' });
+      });
+  });
+  router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    Resources.findResourceById(id)
+      .then(resource => {
+        res.status(200).json(resource);
+      })
+      .catch(err => {
+        res.status(500).json({ message: 'Failed to retrieve the resource' });
+      });
+  });
+  router.post('/', (req, res) => {
+    const resource = req.body;
+    Resources.addResource(resource)
+      .then(id => {
+        res.status(201).json({ created_id: id });
+      })
+      .catch(err => {
+        res.status(500).json({ message: 'Failed to create new resource' });
+      });
+  });
+  router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const resource = req.body;
+    Resources.updateResource(id, resource)
+      .then(count => {
+        res.status(200).json({ message: `${count} resource(s) updated` });
+      })
+      .catch(err => {
+        res.status(500).json({ message: 'Failed to update resource' });
+      });
+  });
+  router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    Resources.deleteResource(id)
+      .then(count => {
+        res.status(200).json({ message: `${count} resource(s) deleted` });
+      })
+      .catch(err => {
+        res.status(500).json({ message: 'Failed to delete resource' });
+      });
+  });
+  module.exports = router;
 
-// You can add more endpoints here for managing resources, etc.
-
-module.exports = router;
+  
