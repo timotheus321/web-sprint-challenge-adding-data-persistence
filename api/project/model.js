0f08c7fcd8db3dd83834eb8311219ac65b283073
Timothy White <bigtball321@yahoo.com>
2023-08-24 17:08:02 -0400

    wip

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,31 @@
-// build your `Project` model here
+const db = require('../../data/dbConfig'); 
+
+function findProjects() {
+  return db('projects');
+}
+
+function addProject(project) {
+    return db('projects').insert(project, 'project_id') 
+      .then(ids => {
+     
+        const project_id = ids[0].project_id; // Extract the project_id
+        console.log("project_id:", project_id); // Log project_id
+        return findProjects().where({ project_id }).first(); // Match using project_id
+      })
+      .then(newProject => {
+        if (!newProject) {
+          throw new Error("Project not found after insert");
+        }
+        return newProject;
+      });
+  }
+  
+  
+  
+  
+  
+
+module.exports = {
+  findProjects,
+  addProject,
+};
